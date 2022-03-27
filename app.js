607349e4d04aef283b67ff731a98f15c7be588be
Jan Frerichs <janfrerichs@mail.de>
2022-03-28 00:07:14 +0200

    add script start

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const MongoStore = require('connect-mongo');
 
 
 // const dbUrl = process.env.DB_URL 
-const dbUrl = process.env.DBURL || 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
 
 mongoose.connect( dbUrl , {
     useNewUrlParser: true,
